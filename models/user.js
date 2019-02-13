70e57b20a584cfc36b3fb656896411c489488cb5
Geralyn Craig <gericraig18@gmail.com>
2019-02-12 16:10:22 -0800

    log in, log out,and register now working

--- models/user.js
+++ models/user.js
@@ -1,12 +1,43 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
-  User.associate = function (models) {
-    // associations can be defined here
-  };
-  return User;
-};
+var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
+
+// create a sequelize instance with our local postgres database information.
+var sequelize = new Sequelize('postgres://postgres@localhost:5432/auth-system');
+
+// setup User model and its fields.
+var User = sequelize.define('users', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    }    
+});
+
+// create all the defined tables in the specified database.
+sequelize.sync()
+    .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
+    .catch(error => console.log('This error occured', error));
+
+// export User model for use in other files.
+module.exports = User;
--- server.js
+++ server.js
@@ -1,45 +1,132 @@
-// dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var morgan = require('morgan');
+var User = require('./models/user');
+
+// invoke an instance of express application.
 var app = express();
 
-// routes
-var AccountRoutes = require('./controllers/account_controller');
+// set our application port
+app.set('port', 9000);
 
-var HomeRoutes = require('./controllers/home_controller');
+// set morgan to log info about our requests for development use.
+app.use(morgan('dev'));
 
-var port = process.env.PORT || 3000;
+// initialize body-parser to parse incoming parameters requests to req.body
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// express
-app.set('view engine', 'ejs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// initialize cookie-parser to allow us access the cookies stored in the browser. 
+app.use(cookieParser());
 
-// session options
+// initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
-    secret: 'some secret here',
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
 }));
 
-// user security
-app.use(session({
-    secret: 'randomstringsessionsecret'
-}));
 
-app.use('/', AccountRoutes.AccountRoutes);
-
-app.use(function(req,res,next){
-    if(req.session.email == null || req.session.email.length ==0 ){
-        res.redirect('/login'); 
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
     }
-    else{
-      next();
-    }
-  });
-  app.use('/',HomeRoutes.HomeRoutes);
+    next();
+});
 
-app.listen(port);
+
+// middleware function to check for logged-in users
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }    
+};
+
+
+// route for Home-Page
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+
+// route for user signup
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html');
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/dashboard');
+        })
+        .catch(error => {
+            res.redirect('/signup');
+        });
+    });
+
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
+
+
+// route for user's dashboard
+app.get('/dashboard', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/public/dashboard.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for user logout
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+});
+
+
+// route for handling 404 requests(unavailable routes)
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+});
+
+
+// start the express server
+app.listen(app.get('port'), () => console.log(`App started on port ${app.get('port')}`));
